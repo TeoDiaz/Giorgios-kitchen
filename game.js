ba1178ed6a3dc8df9b29da1e35c17f55b63d4011
Teo Diaz <tdiazsan@gmail.com>
2018-09-04 16:10:26 +0200

    img desaparecen

--- game.js
+++ game.js
@@ -1,63 +1,70 @@
-document.getElementById("pizza-btn").onclick = function () {
-  var link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.href = 'game-scenario.css';
-  document.head.appendChild(link);
+window.onload = function () {
+  $("#pizza-btn").click(function () {
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'game-scenario.css';
+    document.head.appendChild(link);
 
-  var secondLink = document.createElement('link');
-  secondLink.rel = 'stylesheet';
-  secondLink.href = 'pizza.css';
-  document.head.appendChild(secondLink);
+    var secondLink = document.createElement('link');
+    secondLink.rel = 'stylesheet';
+    secondLink.href = 'pizza.css';
+    document.head.appendChild(secondLink);
 
-  var pizza = new Pizza;
-  var random = pizza.randomPizza();
-  
-  checkName();
-  setInterval(function(){showIngredients()},1000);
+    var pizza = new Pizza;
+    var random = pizza.randomPizza();
 
-  function checkName() {
-    if (random[0].name == "allin") {
-      console.log(random[0].name);
-      document.getElementById("all-in").style.visibility = "visible";
+    checkName();
+    setInterval(function () { showIngredients() }, 1000);
+    var count = 0;
 
+    function checkName() {
+      if (random[0].name == "allin") {
+        console.log(random[0].name);
+        document.getElementById("all-in").style.visibility = "visible";
+
+      }
+      if (random[0].name == "veggie") {
+        console.log(random[0].name);
+        document.getElementById("veggie").style.visibility = "visible";
+
+      }
+      if (random[0].name == "pepperoni") {
+        console.log(random[0].name);
+        document.getElementById("pepperoni").style.visibility = "visible";
+
+      }
+      if (random[0].name == "tuna") {
+        console.log(random[0].name);
+        document.getElementById("tuna").style.visibility = "visible";
+
+      }
+      if (random[0].name == "diabola") {
+        console.log(random[0].name);
+        document.getElementById("diabola").style.visibility = "visible";
+      }
     }
-    if (random[0].name == "veggie") {
-      console.log(random[0].name);
-      document.getElementById("veggie").style.visibility = "visible";
 
+    function showIngredients() {
+      var ingredients = new Ingredients;
+      var randNum = Math.floor(Math.random() * ingredients.images.length);
+      console.log(ingredients.images[randNum])
+      var randomIng = ingredients.images[randNum].img;
+
+      var newIng = document.createElement('img');
+      $(newIng).addClass("ingredients");
+      newIng.src = randomIng;
+      newIng.setAttribute("id", "ingr-img");
+
+
+      $('#ingre').append(newIng);
+
+      count++;
+
+      if (count == 10) {
+        $("#ingre img:first").remove();
+        count = 9;
+      }
+      console.log(count);
     }
-    if (random[0].name == "pepperoni") {
-      console.log(random[0].name);
-      document.getElementById("pepperoni").style.visibility = "visible";
-
-    }
-    if (random[0].name == "tuna") {
-      console.log(random[0].name);
-      document.getElementById("tuna").style.visibility = "visible";
-
-    }
-    if (random[0].name == "diabola") {
-      console.log(random[0].name);
-      document.getElementById("diabola").style.visibility = "visible";
-
-    }
-  }
-
-  function showIngredients() {
-    var ingredients = new Ingredients;
-    var randNum = Math.floor(Math.random()*ingredients.images.length); 
-    console.log(ingredients.images[randNum])
-    var randomIng = ingredients.images[randNum].img;
-    
-    var newIng = document.createElement('img');
-    newIng.className = "ingredients";
-    newIng.src = randomIng;
-    document.getElementById('ingre').appendChild(newIng); 
-    console.log("Diego no va a hacer nada en el Hack Show");
-  }
-};
-
-
-
-
-
+  });
+}
