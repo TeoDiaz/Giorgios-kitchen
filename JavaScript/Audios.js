2f89a7032f07a482dca1f85fffca6233d99277ba
Teo Diaz <tdiazsan@gmail.com>
2018-09-06 19:30:42 +0200

    all audios put

--- JavaScript/Audios.js
+++ JavaScript/Audios.js
@@ -1,8 +1,16 @@
-    var miPiace = new Audio("audios/Mi Piace La Pizza.mp3");
-    var pomodoro = new Audio("audios/oh pomodoro.mp3");
-    var complimenti = new Audio("audios/Complimenti.mp3");
-    var formaggio = new Audio("audios/formaggio.mp3");
-    var cosaFai = new Audio("audios/Cosa Fai.mp3");
-    var benFato = new Audio("audios/BenFato.mp3");
-    var buah = new Audio("audios/Buah.mp3");
-   
+var miPiace = new Audio("audios/Mi Piace La Pizza.mp3");
+var pomodoro = new Audio("audios/oh pomodoro.mp3");
+var complimenti = new Audio("audios/Complimenti.mp3");
+var cosaFai = new Audio("audios/Cosa Fai.mp3");
+var benFato = new Audio("audios/BenFato.mp3");
+var insalata = new Audio("audios/insalata.mp3");
+var manzana = new Audio("audios/manzana.mp3");
+var pepperoni = new Audio("audios/pepperoni.mp3");
+var pescado = new Audio("audios/pescado.mp3");
+var pimienta = new Audio("audios/pimienta.mp3");
+var zanahoria = new Audio("audios/zanahoria.mp3");
+var seta = new Audio("audios/seta.mp3");
+var buah = new Audio("audios/Buah.mp3");
+var queso = new Audio("audios/formaggio.mp3");
+var tocino = new Audio("audios/tocino.mp3");
+var piña = new Audio("audios/piña.mp3");
--- JavaScript/game.js
+++ JavaScript/game.js
@@ -28,21 +28,12 @@ window.onload = function() {
       var num = Math.floor(Math.random() * ingredients.length);
       var idImg = ingredients[num];
       $("<img>")
-          .attr({
-            class: "ingredients",
-            id: idImg,
-            src: randomIng(ingredients[num])
-          })
-          .appendTo("#ingre");
-      if (!selectedIngr.includes(idImg)) {
-        $("<img>")
-          .attr({
-            class: "ingredients",
-            id: idImg,
-            src: randomIng(ingredients[num])
-          })
-          .appendTo("#ingre");
-      }
+        .attr({
+          class: "ingredients",
+          id: idImg,
+          src: randomIng(ingredients[num])
+        })
+        .appendTo("#ingre");
     }
 
     function randomIng(ele) {
@@ -57,28 +48,48 @@ window.onload = function() {
     }
     var selectedIngr = [];
     function makeDesappear() {
-      var elemClick = $(".ingredients:hover").click(function() {
+      var elemClick = $(".ingredients:hover");
+      elemClick.click(function() {
         var imgId = $(elemClick).attr("id");
         if (takePizza.includes(imgId)) {
+          $("#" + imgId).remove();
           $(".ingre-" + imgId).addClass("visible");
           money++;
           $(".score").html("Ganancias: " + money + " $");
           if (!selectedIngr.includes(imgId)) {
             selectedIngr.push(imgId);
             if (imgId == "queso") {
-              formaggio.play();
-              $(elemClick).remove();
+              queso.play();
+            } else if (imgId == "seta") {
+              seta.play();
+            } else if (imgId == "zanahoria") {
+              zanahoria.play();
+            } else if (imgId == "pimienta") {
+              pimienta.play();
+            } else if (imgId == "pescado") {
+              pescado.play();
+            } else if (imgId == "manzana") {
+              manzana.play();
+            }else if(imgId== "pepperoni"){
+              pepperoni.play();
+            }else if(imgId=="ensalada"){
+              insalata.play();
+            }else if(imgId=="tomate"){
+              pomodoro.play();
+            }else if(imgId=="tocino"){
+              tocino.play();
             }
           }
         } else if (imgId == "pizza") {
-          buah.play();  
-          money += 10;
-          $(elemClick).remove();
+          buah.play();
+          $("#" + imgId).remove();
+          money = money + 10;
+          $(".score").html("Ganancias: " + money + " $");
         } else {
           cosaFai.play();
           money--;
+          $(".score").html("Ganancias: " + money + " $");
         }
-        
       });
     }
     function checkWinner() {
